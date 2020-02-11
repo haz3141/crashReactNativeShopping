7d69dab0a13e4e5b0e7a0b7af6507a539be4cacd
haz3141 <38766403+haz3141@users.noreply.github.com>
2020-02-11 08:46:02 -0500

    Added ListItem component & fixed Flow config issue

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 import Header from './components/Header';
+import ListItem from './components/ListItem';
+import {uuid} from 'uuidv4';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Shoes'},
+    {id: uuid(), text: 'Pants'},
+    {id: uuid(), text: 'Shirt'},
+    {id: uuid(), text: 'Socks'},
+  ]);
+
   return (
     <View style={styles.container}>
       <Header />
+      <FlatList
+        data={items}
+        renderItem={({item}) => <ListItem item={item} />}
+      />
     </View>
   );
 };
@@ -13,7 +26,6 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 60,
   },
 });
 
